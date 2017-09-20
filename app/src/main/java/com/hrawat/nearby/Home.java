96b6ccc42cbcebafcc06977667fd00ac9ce20edf
ANJUM <shaheemanjum302@gmail.com>
2017-09-20 13:25:03 +0530

    + test

--- app/src/main/java/com/hrawat/nearby/Home.java
+++ app/src/main/java/com/hrawat/nearby/Home.java
@@ -18,7 +18,7 @@ public class Home extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+            super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
