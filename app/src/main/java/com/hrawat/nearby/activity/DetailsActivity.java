c090bf05003abc85843f01f6568924c3093306e7
hrawat_xavient <harshrock2010@gmail.com>
2017-10-03 11:44:48 +0530

    added DetailsActivity

---
+++ app/src/main/java/com/hrawat/nearby/activity/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.hrawat.nearby.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hrawat.nearby.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
